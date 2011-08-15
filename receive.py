fb891ddad283b317f9050b0edbd689aa9cf0b836
Justin Azoff <justin@bouncybouncy.net>
2011-08-15 13:09:23 -0400

    rework output

--- receive.py
+++ receive.py
@@ -3,28 +3,14 @@ import socket
 import struct
 import sys
 import time
+import threading
+import Queue
 
 message = 'very important data'
 multicast_group = '239.255.52.100'
 PORT=7000
 server_address = ('', PORT)
-
-def send():
-    # Create the datagram socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Set a timeout so the socket does not block indefinitely when trying
-    # to receive data.
-    sock.settimeout(0.2)
-
-    # Set the time-to-live for messages to 1 so they do not go past the
-    # local network segment.
-    ttl = struct.pack('b', 3)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
-    while True:
-        sent = sock.sendto(message, (multicast_group, PORT))
-        time.sleep(1)
+STATS_INTERVAL = 1
 
 def recv(seconds=4):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -40,25 +26,38 @@ def recv(seconds=4):
 
     start = c = s = time.time()
     total = 0
-    all_total = 0
     while c - start < seconds:
         data, address = sock.recvfrom(8192)
         total += len(data)
-        all_total += len(data)
         c = time.time()
-        if c- s >= 1:
+        if c- s >= STATS_INTERVAL:
             kbytes = total/1024
-            mbit = kbytes*8/1024.0
-            if kbytes > 2:
-                print "%d Kbytes in %0.2f seconds %0.2f megabit" % (kbytes, c-s, mbit)
+            mbit = kbytes*8/1024.0/STATS_INTERVAL
+
+            yield c, kbytes, mbit
+
             total=0
             s=c
 
-    kbytes = all_total/1024
-    mbit = kbytes*8/1024.0/(c-start)
-    print "%d Kbytes in %0.2f seconds %0.2f megabit" % (kbytes, c-start, mbit)
+def send_stats(item):
+    print "%s %d Kbytes in %0.2f seconds %0.2f megabit" % (item[0], item[1], STATS_INTERVAL, item[2])
+
+def stats_thread(Q):
+    while True:
+        item = Q.get()
+        if item is None:
+            return
+        send_stats(item)
+
+def run_test(seconds):
+    for x in recv(seconds):
+        send_stats(x)
 
 if __name__ == "__main__":
     import sys
-    seconds = int(sys.argv[1])
-    recv(seconds)
+    seconds = 30
+    try :
+        seconds = int(sys.argv[1])
+    except:
+        pass
+    run_test(seconds)
