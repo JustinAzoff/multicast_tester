1fd2e744b4a2bdb2e54aecdef6b69ecc79e186f9
Justin Azoff <justin@bouncybouncy.net>
2011-08-15 13:17:59 -0400

    send stats

--- receive.py
+++ receive.py
@@ -3,14 +3,20 @@ import socket
 import struct
 import sys
 import time
+
 import threading
 import Queue
+
+import urllib
+import urllib2
 
 message = 'very important data'
 multicast_group = '239.255.52.100'
 PORT=7000
 server_address = ('', PORT)
+
 STATS_INTERVAL = 1
+STATS_URL = "http://mcastserver:7001/send"
 
 def recv(seconds=4):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -41,6 +47,11 @@ def recv(seconds=4):
 
 def send_stats(item):
     print "%s %d Kbytes in %0.2f seconds %0.2f megabit" % (item[0], item[1], STATS_INTERVAL, item[2])
+    data = urllib.urlencode({'time': item[0], 'kbytes': item[1], 'mbits': item[2]})
+    try :
+        urllib2.urlopen(STATS_URL, data, timeout=10).read()
+    except Exception, e:
+        print e
 
 def stats_thread(Q):
     while True:
@@ -50,8 +61,14 @@ def stats_thread(Q):
         send_stats(item)
 
 def run_test(seconds):
-    for x in recv(seconds):
-        send_stats(x)
+    stats_queue = Queue.Queue()
+    t = threading.Thread(target=stats_thread, name="stats", args=(stats_queue,))
+    t.start()
+    for stat in recv(seconds):
+        stats_queue.put(stat)
+
+    stats_queue.put(None)
+    t.join()
 
 if __name__ == "__main__":
     import sys
