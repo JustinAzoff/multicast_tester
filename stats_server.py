802537dacac1761b49e9ac45ed932dcb99693330
Justin Azoff <justin@bouncybouncy.net>
2011-08-15 13:44:45 -0400

    add info page

--- stats_server.py
+++ stats_server.py
@@ -11,6 +11,7 @@ bottle.TEMPLATE_PATH.insert(0, template_dir)
 app = Bottle()
 
 import logging
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("stats-server")
 
 #db stuff
@@ -18,7 +19,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, DateTime, Float, Integer, String, MetaData, ForeignKey
 from sqlalchemy.sql import func
 
-engine = create_engine('sqlite:///stats.db', echo=True)
+engine = create_engine('sqlite:///stats.db', echo=False)
 
 metadata = MetaData()
 stats = Table('stats', metadata,
@@ -41,6 +42,10 @@ def log_stats(ip, kbytes, mbits):
         stats.insert().values(time=func.now(), ip=ip, kbytes=kbytes, mbits=mbits)
     )
 
+def get_stats():
+    conn = engine.connect()
+    return conn.execute(stats.select().order_by(stats.c.time.desc()))
+
 @app.post("/send")
 def send():
     ip = request.environ['REMOTE_ADDR']
@@ -49,8 +54,12 @@ def send():
     log_stats(ip, kbytes, mbits)
     return "ok"
 
+@app.route("/")
+@view("index.mako")
+def index():
+    return dict(stats=get_stats())
+
 def main():
-    logging.basicConfig(level=logging.DEBUG)
     run(app, host='0.0.0.0',server='auto', port=7001)
 
 if __name__ == "__main__":
