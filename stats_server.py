fe184cf3ef6b1cc3a914544e155f626e63df2b00
Justin Azoff <justin@bouncybouncy.net>
2011-08-17 10:28:52 -0400

    better log format

--- stats_server.py
+++ stats_server.py
@@ -15,7 +15,7 @@ bottle.TEMPLATE_PATH.insert(0, template_dir)
 app = Bottle()
 
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger("stats-server")
 
 #db stuff
